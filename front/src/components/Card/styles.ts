8f7ff464b8496a097456f99e2bf7faa52b3e308c
Olivio Freitas <ojfneto92@gmail.com>
2023-05-25 13:04:36 -0300

    feat: card component

---
+++ front/src/components/Card/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.li`
+    width: 250px;
+    height: 100px;
+    background-color: var(--color-gray-300);
+    color: var(--color-gray-900);
+
+    margin: 10px;
+`
