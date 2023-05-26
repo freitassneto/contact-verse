228678c9a5f19f8ae3990a75106e80bd8b9ac469
Olivio Freitas <ojfneto92@gmail.com>
2023-05-26 13:37:16 -0300

    feat: login css ok

--- front/src/pages/Login/index.tsx
+++ front/src/pages/Login/index.tsx
@@ -3,6 +3,8 @@ import { useForm } from "react-hook-form";
 import { LoginData, schema } from "./validator";
 import { useAuth } from "../../hooks/useAuth";
 import { Link } from "react-router-dom";
+import { Container, LoginLeftColumn, LoginRightColumn } from "./styles";
+import logo from "../../assets/logo-contact-verse.png";
 
 export const Login = () => {
   const {
@@ -17,24 +19,47 @@ export const Login = () => {
   const { signIn } = useAuth();
 
   return (
-    <main>
-      <h2>Login</h2>
+    <Container>
+      <main>
+        <LoginLeftColumn>
+          <img src={logo} alt="Logo Contact Verse" />
+          <h3>
+            A agenda digital que mantém seus contatos sempre ao alcance de suas
+            mãos!
+          </h3>
+        </LoginLeftColumn>
+        <LoginRightColumn>
+          <h2>Login</h2>
 
-      <form onSubmit={handleSubmit(signIn)}>
-        <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
-        {errors.email?.message && <small>{errors.email.message}</small>}
+          <form onSubmit={handleSubmit(signIn)}>
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Digite eu e-mail"
+              {...register("email")}
+            />
+            {errors.email?.message && <small>{errors.email.message}</small>}
 
-        <label htmlFor="password">Senha</label>
-        <input type="password" id="password" {...register("password")} />
-        {errors.password?.message && <small>{errors.password.message}</small>}
+            <label htmlFor="password">Senha</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Digite sua senha"
+              {...register("password")}
+            />
+            {errors.password?.message && (
+              <small>{errors.password.message}</small>
+            )}
 
-        <button type="submit">Entrar</button>
-      </form>
-      <section>
-        <h3>Novo por aqui?</h3>
-        <Link to="/register">Cadastre-se</Link>
-      </section>
-    </main>
+            <button type="submit">Entrar</button>
+          </form>
+          <section className="loginSection">
+            <h3>Novo por aqui?</h3>
+            <Link to="/register">Cadastre-se</Link>
+          </section>
+        </LoginRightColumn>
+      </main>
+    </Container>
   );
 };
