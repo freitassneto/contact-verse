bdbf9d51afafcaa9eb25b5740dd8ae7eedfec52b
Olivio Freitas <ojfneto92@gmail.com>
2023-05-29 13:38:35 -0300

    feat: card customization ok

--- front/src/components/Card/styles.ts
+++ front/src/components/Card/styles.ts
@@ -2,10 +2,22 @@ import styled from "styled-components";
 
 export const Container = styled.li`
     width: 300px;
-    height: 100px;
     padding: 20px;
-    
     background-color: var(--color-gray-300);
     color: var(--color-gray-900);
-    border-radius: 4px;
+    border-radius: 8px;
+
+    div {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+
+    div > svg {
+        font-size: 3.0rem;
+    }
+
+    div > h3 {
+        font-weight: 600;
+    }
 `
